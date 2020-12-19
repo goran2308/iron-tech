96bc89531e83908d56c36fda28123f0347fd8887
Goran <solev.goran@gmail.com>
2020-12-19 16:08:32 +0000

    bould services sections and started the customer reviews section

---
+++ public/js/swiper.js
@@ -0,0 +1,30 @@
+const mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  // pagination: {
+  //   el: '.swiper-pagination',
+  // },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  autoplay: {
+    delay: 5000,
+  },
+
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  },
+
+  // And if we need scrollbar
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
+});

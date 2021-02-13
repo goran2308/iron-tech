0f35fab84cfddacc7ad6885fc7d1cd0d76015216
Goran <solev.goran@gmail.com>
2021-02-13 17:28:25 +0000

    finished responsivness and fixed the layout issues

--- public/js/aside.js
+++ public/js/aside.js
@@ -1,9 +1,14 @@
 const cog = document.querySelector('.cog');
 const mobileNav = document.querySelector('aside');
 const closeButton = document.querySelector('.fa-times');
+const navLinks = document.querySelectorAll('.sideNav-links');
 
 cog.addEventListener('click', cogClick);
 closeButton.addEventListener('click', closeClick);
+
+navLinks.forEach(element => {
+  element.addEventListener('click', closeClick);
+});
 
 function cogClick() {
   if (mobileNav.style.display = 'none') {

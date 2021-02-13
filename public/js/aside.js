55ae4bae24d33790f10aeaf2ba848fefe93b6905
Goran <solev.goran@gmail.com>
2021-02-13 23:33:39 +0000

    more SEO

--- public/js/aside.js
+++ public/js/aside.js
@@ -1,3 +1,4 @@
+// Mobile navigation events
 const cog = document.querySelector('.cog');
 const mobileNav = document.querySelector('aside');
 const closeButton = document.querySelector('.fa-times');
@@ -25,3 +26,49 @@ function closeClick() {
     mobileNav.style.display = 'flex'
   }
 };
+
+
+
+// Hide the navigation on scroll down
+(function () {
+
+  const doc = document.documentElement;
+  const w = window;
+
+  let prevScroll = w.scrollY || doc.scrollTop;
+  let curScroll;
+  let direction = 0;
+  let prevDirection = 0;
+
+  let header = document.querySelector('.navbar');
+
+  const checkScroll = function () {
+    curScroll = w.scrollY || doc.scrollTop;
+
+    if (curScroll > prevScroll) {
+      direction = 2;
+    } else if (curScroll < prevScroll) {
+      direction = 1;
+    }
+
+    if (direction !== prevDirection) {
+      toggleHeader(direction, curScroll);
+    }
+
+    prevScroll = curScroll;
+  };
+
+
+  const toggleHeader = function (direction, curScroll) {
+    if (direction === 2 && curScroll > 108) {
+      header.classList.add('hide');
+      prevDirection = direction;
+    } else if (direction === 1) {
+      header.classList.remove('hide');
+      prevDirection = direction;
+    }
+  };
+
+  window.addEventListener('scroll', checkScroll);
+
+})();
